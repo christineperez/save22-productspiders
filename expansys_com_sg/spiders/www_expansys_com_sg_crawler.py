1ca34e0f78afffb2e70592987eea024d6d0b98ef
noelle <ncaperezy@gmail.com>
2016-07-07 04:06:29 -0400

    includes rule in craewler

--- expansys_com_sg/spiders/www_expansys_com_sg_crawler.py
+++ expansys_com_sg/spiders/www_expansys_com_sg_crawler.py
@@ -2,16 +2,26 @@ from scrapy.spider import BaseSpider
 from scrapy.spider import HtmlXpathSelector
 from scrapy,contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.spiders import CrawlSpider, Rule
 from expansys_com_sg.items import ExpansysComSgItem
 
 class WwwExpansysComSgCrawler(BaseSpider):
   name = "www_expansys_com_sg_crawler"
   allowed_domains = ["www.expansys.com.sg"]
-  start_urls = ['http://www.expansys.com.sg/mobile-phones/' , 'http://www.expansys.com.sg/tablet-pcs+ipads/' , 'http://www.expansys.com.sg/accessory-finder/' , 'http://www.expansys.com.sg/audio/' , 'http://www.expansys.com.sg/watches/' , 'http://www.expansys.com.sg/smart-gadget-offers/' , 'http://www.expansys.com.sg/action/cameras/']
+  start_urls = ['http://www.expansys.com.sg/']
   
-  def parse(self, response)
+  rules = (
+    Rule(SgmlLinkExtractor(allow=(r'//?page=[0-9]#listing '), deny = ()), follow=True,callback='parse_item',
+        ),
+        )
+
+  def parse(self, response):
     hxs = HtmlXPathSelector(response)
-    dls = hxs.select('///dl')
-    for dl in dls:
+    tds = hxs.select('')
+    for td in tds:
+  
+  def parse_item(self, response):
+  
+      
        
 
